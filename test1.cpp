3e3f78d3047f09da9f0e3b10a3ae5a92d4134354
Japanese <77375794+gadyintah@users.noreply.github.com>
2021-07-12 02:28:36 +0800

    First Commit
    Commit to main for testing number 1

---
+++ test1.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    fstream in("Random Names.txt");
+    int numLoop, randNum, average, sum, count = 0;
+    int grade[1000];
+    float numTemp;
+
+    in >> randNum;
+
+    while(in >> numLoop){
+        sum = 0;
+        numTemp = 0;
+
+        for(int i=0; i<numLoop; i++){
+            in >> grade[i];
+        } 
+
+        for(int j=0; j<numLoop; j++){
+            sum = sum + grade[j];
+        }
+
+        average = sum/numLoop;
+
+        for(int k=0; k<numLoop; k++){
+            if(grade[k] > average){
+                numTemp++;
+            }
+        }
+
+        count++;
+        cout << fixed << setprecision(3) << "%" << (numTemp/numLoop)*100 << endl;
+
+        if(randNum == count) return 0;
+
+
+    }
+    cout << "Hey";
+}
